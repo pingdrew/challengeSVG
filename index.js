e786b25fd75ce67a4ea6e8e7736618f5364d9878
pingdrew <pingdrew0@gmail.com>
2023-07-27 20:30:39 -0500

    completed all functionality, deleted two files, working on tests

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
+// Importing command line questions
 const CLI = require('./lib/cli.js');
+
+// Importing classes from ./lib/shapes directory
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
 const cli = new CLI();
 
-cli.questions();
-
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
+cli.questions()
